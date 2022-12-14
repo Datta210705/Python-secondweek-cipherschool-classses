02648690237547c53787a83b8eb032268dcb1250
Datta210705 <118921551+Datta210705@users.noreply.github.com>
2022-12-14 19:24:27 +0530

    Add files via upload

---
+++ class12.py
@@ -0,0 +1,6 @@
+a = 5
+print("value of a is %d" % (a))
+
+name = "Datta"
+company = "india"
+print("name = {name} company = {company}".format(name = name , company = company))
---
+++ class17.py
@@ -0,0 +1,14 @@
+#object oriented programme
+student1 = {"name":"Datta","marks":60}
+student2 = {"name":"Sam","marks":50}
+print()
+
+class Person:
+    pass
+
+p = Person()
+print(p)
+
+a =1 
+def square(a):
+    print(a**2)
